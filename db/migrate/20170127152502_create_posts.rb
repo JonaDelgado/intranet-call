deee677373724e96ca23276914f9df333dd1c213
JonaDelgado <jonathan.delgado25@gmail.com>
2017-02-02 16:30:33 -0300

    Se mejora posts para ingreso de feedback, se agrega campo ejecutivo y motivo, además se quitan acceso a aliminar y/o editar feedback realizados

--- db/migrate/20170127152502_create_posts.rb
+++ db/migrate/20170127152502_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :description
+      t.string :motivo
+      t.string :ejecutivo
+      t.text :descripcion
 
       t.timestamps
     end
